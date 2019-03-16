3839a77596dbc00d89f53115a0d076eca4e3222b
antimony51122 <zhuyaosheng51122@gmail.com>
2019-03-16 20:21:19 +0000

    re-formatting

--- Assets/_Audio_Visualizer/Scripts/AudioVisualizer.cs
+++ Assets/_Audio_Visualizer/Scripts/AudioVisualizer.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-public class AudioVisualizer : MonoBehaviour
-{
+public class AudioVisualizer : MonoBehaviour {
     public GameObject[] Objects;
     public float Scale = 10f;
-    public enum EnumType {X, Y, Z}
+
+    public enum EnumType {
+        X,
+        Y,
+        Z
+    }
+
     public EnumType Type = EnumType.Y;
 
     private float[] Samples = new float[1024];
@@ -15,34 +20,31 @@ public class AudioVisualizer : MonoBehaviour
     FFTWindow fftWindow;
     AudioSource thisAudioSource;
 
-    void Start()
-    {
+    void Start() {
         thisAudioSource = GetComponent<AudioSource>();
         fftWindow = FFTWindow.BlackmanHarris;
     }
 
 
-    void Update()
-    {
+    void Update() {
         thisAudioSource.GetSpectrumData(Samples, 0, fftWindow);
 
-        for (int i = 0; i < Objects.Length; i++)
-        {
+        for (int i = 0; i < Objects.Length; i++) {
             var myScale = Samples[i] * Scale;
 
-            if(Type == EnumType.X)
-            {
-                Objects[i].transform.localScale = new Vector3(myScale, Objects[i].transform.localScale.y, Objects[i].transform.localScale.z);
+            if (Type == EnumType.X) {
+                Objects[i].transform.localScale = new Vector3(myScale, Objects[i].transform.localScale.y,
+                    Objects[i].transform.localScale.z);
             }
 
-            if (Type == EnumType.Y)
-            {
-                Objects[i].transform.localScale = new Vector3(Objects[i].transform.localScale.x, myScale, Objects[i].transform.localScale.z);
+            if (Type == EnumType.Y) {
+                Objects[i].transform.localScale = new Vector3(Objects[i].transform.localScale.x, myScale,
+                    Objects[i].transform.localScale.z);
             }
 
-            if (Type == EnumType.Z)
-            {
-                Objects[i].transform.localScale = new Vector3(Objects[i].transform.localScale.x, Objects[i].transform.localScale.y, myScale);
+            if (Type == EnumType.Z) {
+                Objects[i].transform.localScale = new Vector3(Objects[i].transform.localScale.x,
+                    Objects[i].transform.localScale.y, myScale);
             }
         }
     }
