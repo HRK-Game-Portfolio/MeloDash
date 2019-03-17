b745a974d05835f7c86112fb4b0891fa576e394b
antimony51122 <zhuyaosheng51122@gmail.com>
2019-03-17 17:52:20 +0000

    beat detection assets imported

---
+++ Assets/_Beat_Detection/Event - Example/EventExample.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventExample : MonoBehaviour {
+    public GameObject AudioBeat;
+    public GUIText energy;
+    public GUIText kick;
+    public GUIText snare;
+    public GUIText hithat;
+
+    public GameObject genergy;
+    public GameObject gkick;
+    public GameObject gsnare;
+    public GameObject ghithat;
+
+    public Material matOn;
+    public Material matOff;
+
+    public void MyCallbackEventHandler(BeatDetection.EventInfo eventInfo) {
+        switch (eventInfo.messageInfo) {
+            case BeatDetection.EventType.Energy:
+                StartCoroutine(showText(energy, genergy));
+                break;
+            case BeatDetection.EventType.HitHat:
+                StartCoroutine(showText(hithat, ghithat));
+                break;
+            case BeatDetection.EventType.Kick:
+                StartCoroutine(showText(kick, gkick));
+                break;
+            case BeatDetection.EventType.Snare:
+                StartCoroutine(showText(snare, gsnare));
+                break;
+        }
+    }
+
+    private IEnumerator showText(GUIText texto, GameObject objeto) {
+        texto.enabled = true;
+        objeto.GetComponent<Renderer>().material = matOn;
+        yield return new WaitForSeconds(0.05f * 3);
+        texto.enabled = false;
+        objeto.GetComponent<Renderer>().material = matOff;
+        yield break;
+    }
+
+    // Use this for initialization
+    void Start() {
+        //Register the beat callback function
+        AudioBeat.GetComponent<BeatDetection>().CallBackFunction = MyCallbackEventHandler;
+    }
+}
---
+++ Assets/_Beat_Detection/Stage Scene/StageController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StageController : MonoBehaviour {
+    public GameObject lightning, leftfocus, rightfocus, pinkflare, blueflare, vlight;
+    public GameObject startButton;
+    public Sprite focuson;
+    public float duration = 0.2f;
+    public List<Color> focusColors = new List<Color>();
+    int currentColor = 0;
+
+    public void MyCallbackEventHandler(BeatDetection.EventInfo eventInfo) {
+        switch (eventInfo.messageInfo) {
+            case BeatDetection.EventType.Energy:
+                RotateColor();
+                PinkflareOn();
+                break;
+            case BeatDetection.EventType.HitHat:
+                LightningOn();
+                break;
+            case BeatDetection.EventType.Kick:
+                VLightOn();
+                break;
+            case BeatDetection.EventType.Snare:
+                BlueflareOn();
+                break;
+        }
+    }
+
+    void Start() {
+        //Register the beat callback function
+        GetComponent<BeatDetection>().CallBackFunction = MyCallbackEventHandler;
+    }
+
+    public void GoStage() {
+        startButton.SetActive(false);
+        GetComponent<AudioSource>().Play();
+    }
+
+    void RotateColor() {
+        currentColor++;
+        currentColor %= focusColors.Count;
+        leftfocus.GetComponent<Image>().color = focusColors[currentColor];
+        rightfocus.GetComponent<Image>().color = focusColors[currentColor];
+    }
+
+    void LightningOn() {
+        if (lightning.activeSelf)
+            return;
+        lightning.SetActive(true);
+        Invoke("LightningOff", duration);
+    }
+
+    void LightningOff() {
+        lightning.SetActive(false);
+    }
+
+    void PinkflareOn() {
+        if (pinkflare.activeSelf)
+            return;
+        pinkflare.SetActive(true);
+        Invoke("PinkflareOff", duration);
+    }
+
+    void PinkflareOff() {
+        pinkflare.SetActive(false);
+    }
+
+    void BlueflareOn() {
+        if (blueflare.activeSelf)
+            return;
+        blueflare.SetActive(true);
+        Invoke("BlueflareOff", duration);
+    }
+
+    void BlueflareOff() {
+        blueflare.SetActive(false);
+    }
+
+    void VLightOn() {
+        if (vlight.activeSelf)
+            return;
+        vlight.SetActive(true);
+        Invoke("VLightOff", duration);
+    }
+
+    void VLightOff() {
+        vlight.SetActive(false);
+    }
+}
